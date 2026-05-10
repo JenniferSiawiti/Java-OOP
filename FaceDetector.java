4999b39396cc8edd641568508eb9ac5504f26d97
Bezallea <soakaluneb@gmail.com>
2026-05-10 22:49:42 +0700

    Implement face detection using OpenCV

--- FaceDetector.java
+++ FaceDetector.java
@@ -1,1 +1,50 @@
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 
+public class FaceDetector {
+
+    private CascadeClassifier faceCascade;
+
+    public FaceDetector() {
+
+        faceCascade =
+                new CascadeClassifier(
+                        "haarcascade_frontalface_default.xml"
+                );
+    }
+
+    public FaceData detectFace(Mat frame) {
+
+        MatOfRect faces = new MatOfRect();
+
+        faceCascade.detectMultiScale(frame, faces);
+
+        if (faces.toArray().length == 0) {
+
+            System.out.println("No face detected!");
+
+            return null;
+        }
+
+        Rect rect = faces.toArray()[0];
+
+        Imgproc.rectangle(
+                frame,
+                new Point(rect.x, rect.y),
+                new Point(rect.x + rect.width,
+                          rect.y + rect.height),
+                new Scalar(0, 255, 0),
+                2
+        );
+
+        System.out.println("Face detected!");
+
+        return new FaceData(
+                rect.x,
+                rect.y,
+                rect.width,
+                rect.height
+        );
+    }
+}
