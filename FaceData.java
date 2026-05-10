6da784c78b1df9fc7108c2f4a6539f9e4f29d808
illenashaws <51033732+illenashaws@users.noreply.github.com>
2026-05-10 14:45:37 +0700

    Add FaceData class for storing facial feature inputs
    Added FaceData class with properties for facial movements.

--- FaceData.java
+++ FaceData.java
@@ -1,1 +1,23 @@
+public class FaceData {
+    private double eyeMovement;
+    private double mouthMovement;
+    private double eyebrowPosition;
 
+    public FaceData(double eyeMovement, double mouthMovement, double eyebrowPosition) {   // its the constructor y'all!!
+        this.eyeMovement = eyeMovement;
+        this.mouthMovement = mouthMovement;
+        this.eyebrowPosition = eyebrowPosition;
+    }
+
+    public double getEyeMovement() {
+        return eyeMovement;
+    }
+
+    public double getMouthMovement() {
+        return mouthMovement;
+    }
+
+    public double getEyebrowPosition() {
+        return eyebrowPosition;
+    }
+} 
