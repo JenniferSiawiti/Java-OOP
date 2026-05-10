0a67a17a2f226fe04d258980d897c7982d06dfa0
illenashaws <51033732+illenashaws@users.noreply.github.com>
2026-05-10 14:41:28 +0700

    Implement micro-expression analysis logic using facial data

--- ExpressionAnalyzer.java
+++ ExpressionAnalyzer.java
@@ -1,1 +1,35 @@
+public class ExpressionAnalyzer {
+    public ExpressionResult analyze(FaceData data) {
+        double eye = data.getEyeMovement();
+        double mouth = data.getMouthMovement();
+        double brow = data.getEyebrowPosition();
 
+        if (eye > 0.85 && mouth > 0.5) { // Surprised
+            double confidence = round((eye + mouth) / 2);
+            return new ExpressionResult("Surprised", confidence);
+        }
+
+        else if (brow > 0.8) { // Angry
+            double confidence = round(brow);
+            return new ExpressionResult("Angry", confidence);
+        }
+
+        else if (mouth > 0.7 && eye > 0.6) { // Happy
+            double confidence = round((mouth + eye) / 2);
+            return new ExpressionResult("Happy", confidence);
+        }
+
+        else if (eye < 0.3 && mouth < 0.3 && brow < 0.3) { // Neutral
+            return new ExpressionResult("Neutral", 0.5);
+        }
+
+        else { // Sad
+            double confidence = round(1 - mouth);
+            return new ExpressionResult("Sad", confidence);
+        }
+    }
+
+    private double round(double value) {
+        return Math.round(value * 100.0) / 100.0;
+    }
+}
