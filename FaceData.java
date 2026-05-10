580e059d7b74391d8b966f238784a0ba38e31bd8
Bezallea <soakaluneb@gmail.com>
2026-05-10 22:57:44 +0700

    Enhance FaceData with dimensions and print method
    Updated FaceData constructor to include dimensions and added printData method for displaying face attributes.

--- FaceData.java
+++ FaceData.java
@@ -1,12 +1,44 @@
 public class FaceData {
+
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
     private double eyeMovement;
     private double mouthMovement;
     private double eyebrowPosition;
 
-    public FaceData(double eyeMovement, double mouthMovement, double eyebrowPosition) {   // its the constructor y'all!!
+    // Constructornya yuh
+    public FaceData(int x, int y, int width, int height,
+                    double eyeMovement,
+                    double mouthMovement,
+                    double eyebrowPosition) {
+
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
         this.eyeMovement = eyeMovement;
         this.mouthMovement = mouthMovement;
         this.eyebrowPosition = eyebrowPosition;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
     }
 
     public double getEyeMovement() {
@@ -20,4 +52,17 @@ public class FaceData {
     public double getEyebrowPosition() {
         return eyebrowPosition;
     }
-} 
+
+    public void printData() {
+
+        System.out.println("Face Coordinates:");
+        System.out.println("X: " + x);
+        System.out.println("Y: " + y);
+        System.out.println("Width: " + width);
+        System.out.println("Height: " + height);
+
+        System.out.println("Eye Movement: " + eyeMovement);
+        System.out.println("Mouth Movement: " + mouthMovement);
+        System.out.println("Eyebrow Position: " + eyebrowPosition);
+    }
+}
